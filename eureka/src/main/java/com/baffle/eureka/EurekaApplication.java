3bb64322637d99f8c640a3bc9512cc821d6d5ad5
kongqh <614988159@qq.com>
2020-05-18 01:12:07 +0800

    初稿

---
+++ eureka/src/main/java/com/baffle/eureka/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.baffle.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+/** 开启Eureka的功能*/
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
